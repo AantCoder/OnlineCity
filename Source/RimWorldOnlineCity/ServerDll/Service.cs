a7703a2776ab953f3b0a43198e0cbf026e3f11bd
Roman8x <Roman8x@gmail.com>
2019-11-14 22:57:43 +0500

    Refactoring: remove PostingChat to single class add Discord command to PostingChatService

--- Source/RimWorldOnlineCity/ServerDll/Service.cs
+++ Source/RimWorldOnlineCity/ServerDll/Service.cs
@@ -4,17 +4,16 @@ using OCUnion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using Transfer;
-using Util;
 
 namespace OCServer
 {
     public class Service
     {
-        public PlayerServer Player;
-        
+        private Services.PostingChat _postingChat = new Services.PostingChat();
+        private PlayerServer _player;
+        public PlayerServer Player { get { return _player; }}
+
         public ModelStatus Login(ModelLogin packet)
         {
             if (Player != null) return null;
@@ -27,7 +26,7 @@ namespace OCServer
                 .FirstOrDefault(p => p.Public.Login == packet.Login);
             if (player != null)
             {
-                if (player.Pass != packet.Pass) //todo
+                if (player.Pass != packet.Pass) //todo ??
                     player = null;
             }
 
@@ -40,7 +39,7 @@ namespace OCServer
                 };
             }
 
-            Player = player;
+            _player = player;
             return new ModelStatus()
             {
                 Status = 0,
@@ -81,14 +80,14 @@ namespace OCServer
 
             Repository.GetData.PlayersAll.Add(player);
             Repository.Get.ChangeData = true;
-            Player = player;
+            _player = player;
             return new ModelStatus()
             {
                 Status = 0,
                 Message = null
             };
         }
-        
+
         public ModelInfo GetInfo(ModelInt packet)
         {
             if (Player == null) return null;
@@ -131,7 +130,7 @@ namespace OCServer
                 }
             }
         }
-        
+
         public ModelStatus CreatingWorld(ModelCreateWorld packet)
         {
             if (Player == null) return null;
@@ -308,7 +307,7 @@ namespace OCServer
                     toClient.WObjectsToDelete = outWOD;
                     Player.LastUpdateTime = timeNow;
                 }
-                
+
                 //прикрепляем письма
                 toClient.Mails = Player.Mails;
                 Player.Mails = new List<ModelMailTrade>();
@@ -346,7 +345,7 @@ namespace OCServer
             lock (toPlayer)
             {
                 toPlayer.Mails.Add(packet);
-            }   
+            }
             return new ModelStatus()
             {
                 Status = 0,
@@ -431,295 +430,10 @@ namespace OCServer
         {
             if (Player == null) return null;
 
-            lock (Player)
+            lock (_player)
             {
-                var timeNow = DateTime.UtcNow;
-                if (string.IsNullOrEmpty(pc.Message))
-                    return new ModelStatus()
-                    {
-                        Status = 0,
-                        Message = null
-                    };
-
-                var chat = Player.Chats.FirstOrDefault(ct => ct.Id == pc.ChatId);
-                if (chat == null)
-                    return new ModelStatus()
-                    {
-                        Status = 1,
-                        Message = "Chat not available"
-                    };
-
-                if (pc.Message[0] == '/')
-                {
-                    var s = pc.Message.Split(new char[] { ' ' }, 2);
-                    var command = s[0].Trim().ToLower();
-                    var args = s.Length == 1 ? "" : s[1];
-                    //разбираем аргументы в кавычках '. Удвоенная кавычка указывает на её символ.
-                    var argsM = SplitBySpace(args);
-                    
-                    // обработка команд чата {
-                    switch (command)
-                    {
-                        case "/help":
-                            PostCommandPrivatPostActivChat(chat, ChatHelpText
-                                + (Player.IsAdmin ? ChatHelpTextAdmin : "" ));
-                            break;
-                        case "/createchat":
-                            Loger.Log("Server createChat");
-                            if (argsM.Count < 1) PostCommandPrivatPostActivChat(chat, "No new channel name specified");
-                            else
-                            {
-                                var nChat = new Chat()
-                                {
-                                    Name = argsM[0],
-                                    OwnerLogin = Player.Public.Login,
-                                    OwnerMaker = true,
-                                    PartyLogin = new List<string>() { Player.Public.Login, "system" },
-                                    Id = Repository.GetData.GetChatId(),
-                                };
-                                nChat.Posts.Add(new ChatPost()
-                                {
-                                    Time = DateTime.UtcNow,
-                                    Message = "User " + Player.Public.Login + " created a channel " + argsM[0],
-                                    OwnerLogin = "system"
-                                });
-                                Player.Chats.Add(nChat);
-
-                                if (argsM.Count > 1)
-                                    PostCommandAddPlayer(nChat, argsM[1]);
-                                Repository.Get.ChangeData = true;
-                            }
-                            break;
-                        case "/exitchat":
-                            Loger.Log("Server exitChat OwnerMaker=" + (chat.OwnerMaker ? "1" : "0"));
-                            if (!chat.OwnerMaker) PostCommandPrivatPostActivChat(chat, "From a shared channel, you can not leave");
-                            else
-                            {
-                                chat.Posts.Add(new ChatPost()
-                                {
-                                    Time = DateTime.UtcNow,
-                                    Message = "User " + Player.Public.Login + " left the channel.",
-                                    OwnerLogin = "system"
-                                });
-                                chat.PartyLogin.Remove(Player.Public.Login);
-                                var r = Player.Chats.Remove(chat);
-                                Loger.Log("Server exitChat remove" + (r ? "1" : "0"));
-                                Repository.Get.ChangeData = true;
-                            }
-                            break;
-                        case "/renamechat":
-                            if (!chat.OwnerMaker) PostCommandPrivatPostActivChat(chat, "You can not rename a shared channel");
-                            if (argsM.Count < 1) PostCommandPrivatPostActivChat(chat, "No new name specified");
-                            else if (chat.OwnerLogin != Player.Public.Login && !Player.IsAdmin)
-                                PostCommandPrivatPostActivChat(chat, "Operation is not available to you");
-                            else
-                            {
-                                chat.Posts.Add(new ChatPost()
-                                {
-                                    Time = DateTime.UtcNow,
-                                    Message = "The channel was renamed to " + argsM[0],
-                                    OwnerLogin = "system"
-                                });
-                                Loger.Log("Server renameChat " + chat.Name + " -> " + argsM[0]);
-                                chat.Name = argsM[0];
-                                Repository.Get.ChangeData = true;
-                            }
-                            break;
-                        case "/addplayer":
-                            Loger.Log("Server addPlayer");
-                            if (argsM.Count < 1) PostCommandPrivatPostActivChat(chat, "Player name is empty");
-                            else PostCommandAddPlayer(chat, argsM[0]);
-                            break;
-                        case "/killmyallplease":
-                            Loger.Log("Server killmyallplease OwnerMaker=" + (chat.OwnerMaker ? "1" : "0"));
-                            if (chat.OwnerMaker) PostCommandPrivatPostActivChat(chat, "Operation only for the shared channel");
-                            else
-                            {
-                                chat.Posts.Add(new ChatPost()
-                                {
-                                    Time = DateTime.UtcNow,
-                                    Message = "User " + Player.Public.Login + " deleted settlements.",
-                                    OwnerLogin = "system"
-                                });
-                                var data = Repository.GetData;
-                                lock (data)
-                                {
-                                    for (int i = 0; i < data.WorldObjects.Count; i++)
-                                    {
-                                        var item = data.WorldObjects[i];
-                                        if (item.LoginOwner != Player.Public.Login) continue;
-                                        //удаление из базы
-                                        item.UpdateTime = timeNow;
-                                        data.WorldObjects.Remove(item);
-                                        data.WorldObjectsDeleted.Add(item);
-                                    }
-                                }
-                                Player.SaveDataPacket = null;
-                                Loger.Log("Server killmyallplease " + Player.Public.Login);
-                                Player = null;
-                                Repository.Get.ChangeData = true;
-                            }
-                            break;
-                        case "/killhimplease":
-                            Loger.Log("Server killhimplease OwnerMaker=" + (chat.OwnerMaker ? "1" : "0"));
-                            if (!Player.IsAdmin) PostCommandPrivatPostActivChat(chat, "Command only for admin");
-                            else
-                            if (argsM.Count < 1) PostCommandPrivatPostActivChat(chat, "Player name is empty");
-                            else
-                            {
-                                var killPlayer = Repository.GetData.PlayersAll
-                                    .FirstOrDefault(p => p.Public.Login == argsM[0]);
-                                if (killPlayer == null)
-                                    PostCommandPrivatPostActivChat(chat, "User " + argsM[0] + " not found");
-                                else
-                                {
-                                    chat.Posts.Add(new ChatPost()
-                                    {
-                                        Time = DateTime.UtcNow,
-                                        Message = "User " + killPlayer.Public.Login + " deleted settlements.",
-                                        OwnerLogin = "system"
-                                    });
-                                    var data = Repository.GetData;
-                                    lock (data)
-                                    {
-                                        for (int i = 0; i < data.WorldObjects.Count; i++)
-                                        {
-                                            var item = data.WorldObjects[i];
-                                            if (item.LoginOwner != killPlayer.Public.Login) continue;
-                                            //удаление из базы
-                                            item.UpdateTime = timeNow;
-                                            data.WorldObjects.Remove(item);
-                                            data.WorldObjectsDeleted.Add(item);
-                                        }
-                                    }
-                                    killPlayer.SaveDataPacket = null;
-                                    Repository.Get.ChangeData = true;
-                                    Loger.Log("Server killhimplease " + killPlayer.Public.Login);
-                                }
-                            }
-                            break;
-                        default:
-                            PostCommandPrivatPostActivChat(chat, "Command not found: " + command);
-                            return new ModelStatus()
-                            {
-                                Status = 2,
-                                Message = "Command not found: " + command
-                            };
-                    }
-                    // } обработка команд чата
-
-                }
-                else
-                {
-                    Loger.Log("Server post " + Player.Public.Login /*+ " " + timeNow.Ticks*/ + ":" + pc.Message);
-                    var mmsg = pc.Message;
-                    if (mmsg.Length > 2048) mmsg = mmsg.Substring(0, 2048);
-                    chat.Posts.Add(new ChatPost()
-                    {
-                        Time = timeNow,
-                        Message = mmsg,
-                        OwnerLogin = Player.Public.Login
-                    });
-                }
-
-                return new ModelStatus()
-                {
-                    Status = 0,
-                    Message = null
-                };
+                return _postingChat.GetModelStatus(ref _player, pc);
             }
-        }
-
-        private void PostCommandPrivatPostActivChat(Chat chat, string msg)
-        {
-            chat.Posts.Add(new ChatPost()
-            {
-                Time = DateTime.UtcNow,
-                Message = msg,
-                OwnerLogin = "system",
-                OnlyForPlayerLogin = Player.Public.Login
-            });
-        }
-
-        private void PostCommandAddPlayer(Chat chat, string who)
-        {
-            if (chat.PartyLogin.Any(p => p == who)) 
-            {
-                PostCommandPrivatPostActivChat(chat, "The player is already here");
-            }
-            else
-            {
-                var newPlayer = Repository.GetData.PlayersAll
-                    .FirstOrDefault(p => p.Public.Login == who);
-                if (newPlayer == null)
-                    PostCommandPrivatPostActivChat(chat, "User " + who + " not found");
-                else if (!Player.PublicChat.PartyLogin.Any(p => p == who))
-                    PostCommandPrivatPostActivChat(chat, "Can not access " + who + " player");
-                else if (!chat.OwnerMaker)
-                    PostCommandPrivatPostActivChat(chat, "People can not be added to a shared channel");
-                else if (chat.OwnerLogin != Player.Public.Login && !Player.IsAdmin)
-                    PostCommandPrivatPostActivChat(chat, "You can not add people");
-                else
-                {
-                    chat.Posts.Add(new ChatPost()
-                    {
-                        Time = DateTime.UtcNow,
-                        Message = "User " + newPlayer.Public.Login + " entered the channel.",
-                        OwnerLogin = "system"
-                    });
-                    chat.PartyLogin.Add(newPlayer.Public.Login);
-                    newPlayer.Chats.Add(chat);
-                    Repository.Get.ChangeData = true;
-                }
-            }
-        }
-        
-        public static List<string> SplitBySpace(string args)
-        {
-            int i = 0;
-            var argsM = new List<string>();
-            while (i + 1 < args.Length)
-            {
-                if (args[i] == '\'')
-                {
-                    int endK = i;
-                    bool exit;
-                    do //запускаем поиск след кавычки снова, если после найденной ещё одна
-                    {
-                        exit = true;
-                        endK = args.IndexOf('\'', endK + 1);
-                        if (endK >= 0 && endK + 1 < args.Length && args[endK + 1] == '\'')
-                        {
-                            //это двойная кавычка - пропускаем её
-                            endK++;
-                            exit = false;
-                        }
-                    }
-                    while (!exit);
-
-                    if (endK >= 0)
-                    {
-                        argsM.Add(args.Substring(i + 1, endK - i - 1).Replace("''", "'"));
-                        i = endK + 1;
-                        continue;
-                    }
-                }
-                var ni = args.IndexOf(" ", i);
-                if (ni >= 0)
-                {
-                    //условие недобавления для двойного пробела
-                    if (ni > i) argsM.Add(args.Substring(i, ni - i));
-                    i = ni + 1;
-                    continue;
-                }
-                else break;
-            }
-            if (i < args.Length)
-            {
-                argsM.Add(args.Substring(i));
-            }
-
-            return argsM;
         }
 
         public ModelStatus ExchengeEdit(OrderTrade order)
@@ -908,6 +622,7 @@ namespace OCServer
                         res.ErrorText = "Player not found";
                         return res;
                     }
+
                     if (hostPlayer.AttackData != null)
                     {
                         res.ErrorText = "The player participates in the attack";
@@ -959,19 +674,5 @@ namespace OCServer
                 return res;
             }
         }
-
-        private const string ChatHelpText = @"Available commands: 
-/help
-/createChat
-/exitChat
-/addPlayer
-/renameChat
-
-";
-        private const string ChatHelpTextAdmin = @"/killmyallplease
-/killhimplease
-
-";
-
     }
 }
