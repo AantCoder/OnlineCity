0db5877c4f7f425ed63796164772737f25ae88ef
Roman8x <Roman8x@gmail.com>
2020-02-11 20:54:14 +0500

    add icons that depends from the wealth of settlement

--- Source/RimWorldOnlineCity/GameClasses/BaseOnline.cs
+++ Source/RimWorldOnlineCity/GameClasses/BaseOnline.cs
@@ -18,6 +18,9 @@ namespace RimWorldOnlineCity
 
         private Material MatColonyOff;
 
+        private Texture2D ColonyOnExpandingTexture;
+        private Texture2D ColonyOffExpandingTexture;
+
         private static Texture2D ColonyOn;
 
         private static Texture2D ColonyOff;
@@ -26,14 +29,29 @@ namespace RimWorldOnlineCity
 
         private static Texture2D ColonyOffExpanding;
 
+        private static WealthTexture[] WealthTexturesOn;
+        private static WealthTexture[] WealthTexturesOff;
+
+        private static int[] WealthLevels;
+
         static BaseOnline()
         {
             ColonyOn = ContentFinder<Texture2D>.Get("ColonyOn");
             ColonyOff = ContentFinder<Texture2D>.Get("ColonyOff");
             ColonyOnExpanding = ContentFinder<Texture2D>.Get("ColonyOnExpanding");
             ColonyOffExpanding = ContentFinder<Texture2D>.Get("ColonyOffExpanding");
+
+            WealthLevels = new int[] { 0, 10_000, 50_000, 100_000, 200_000, 300_000, 500_000, 1_000_000, 2_000_000 };
+            WealthTexturesOn = new WealthTexture[WealthLevels.Length];
+            WealthTexturesOff = new WealthTexture[WealthLevels.Length];
+
+            for (int i = 0; i < WealthLevels.Length; i++)
+            {
+                WealthTexturesOn[i] = new WealthTexture() { Wealth = WealthLevels[i], Texture = ContentFinder<Texture2D>.Get("ColonyOnExpanding" + i.ToString()) };
+                WealthTexturesOff[i] = new WealthTexture() { Wealth = WealthLevels[i], Texture = ContentFinder<Texture2D>.Get("ColonyOffExpanding" + i.ToString()) };
+            }
         }
-        
+
         public override Material Material
         {
             get
@@ -44,18 +62,28 @@ namespace RimWorldOnlineCity
                         , ShaderDatabase.WorldOverlayTransparentLit
                         , Color.white
                         , WorldMaterials.WorldObjectRenderQueue);
-                    //MaterialPool.MatFrom(ColonyOn);
-                    //MaterialPool.MatFrom(base.Faction.def.homeIconPath, ShaderDatabase.WorldOverlayTransparentLit, base.Faction.Color, WorldMaterials.WorldObjectRenderQueue);
+                    if (this.MatColonyOn == null)
+                    {
+                        Loger.Log("this.MatColonyOn == null");
+                    }
+                    else
+                        Loger.Log("this.MatColonyOn != null");
                     return this.MatColonyOn;
                 }
+
+                if (this.MatColonyOff == null) this.MatColonyOff = MaterialPool.MatFrom(ColonyOff
+                    , ShaderDatabase.WorldOverlayTransparentLit
+                    , Color.white
+                    , WorldMaterials.WorldObjectRenderQueue);
+
+                if (this.MatColonyOff == null)
+                {
+                    Loger.Log("this.MatColonyOff == null");
+                }
                 else
-                {
-                    if (this.MatColonyOff == null) this.MatColonyOff = MaterialPool.MatFrom(ColonyOff
-                        , ShaderDatabase.WorldOverlayTransparentLit
-                        , Color.white
-                        , WorldMaterials.WorldObjectRenderQueue);
-                    return this.MatColonyOff;
-                }
+                    Loger.Log("this.MatColonyOff != null");
+
+                return this.MatColonyOff;
             }
         }
 
@@ -63,34 +91,44 @@ namespace RimWorldOnlineCity
         {
             get
             {
-                return IsOnline ? ColonyOnExpanding : ColonyOffExpanding;
+                if (IsOnline)
+                {
+                    if (ColonyOnExpandingTexture == null)
+                    {
+                        ColonyOnExpandingTexture = GetMaterialByWealth(WealthTexturesOn);
+                    }
+
+                    return ColonyOnExpandingTexture;
+                }
+
+                if (ColonyOffExpandingTexture == null)
+                {
+                    ColonyOffExpandingTexture = GetMaterialByWealth(WealthTexturesOff);
+                }
+
+                return ColonyOffExpandingTexture;
             }
         }
+
+        private Texture2D GetMaterialByWealth(WealthTexture[] wealthTextures)
+        {
+            for (int i = 1; i < WealthLevels.Length - 1; i++)
+            {
+                if (this.OnlineWObject.MarketValue < wealthTextures[i].Wealth)
+                {
+                    return wealthTextures[i].Texture;
+                }
+            }
+
+            return wealthTextures[WealthLevels.Length - 1].Texture;
+        }
+
         #endregion
-        /*
-        private Material cachedMat;
-        public override Texture2D ExpandingIcon
-        {
-            get
-            {
-                return base.Faction.def.ExpandingIconTexture;
-            }
-        }
+    }
 
-        public override Material Material
-        {
-            get
-            {
-                if (this.cachedMat == null)
-                {
-                    this.cachedMat = MaterialPool.MatFrom(base.Faction.def.homeIconPath
-                        , ShaderDatabase.WorldOverlayTransparentLit
-                        , Color.blue
-                        , WorldMaterials.WorldObjectRenderQueue);
-                }
-                return this.cachedMat;
-            }
-        }
-        */
+    public class WealthTexture
+    {
+        public int Wealth;
+        public Texture2D Texture;
     }
 }
