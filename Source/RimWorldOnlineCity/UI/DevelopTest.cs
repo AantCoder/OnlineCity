4183d6f5682d2cbd690c7c130ba519965252a6f7
Aant <emAnt@mail.ru>
2020-08-03 00:49:16 +0400

    Version 0.03.45a from 2020.08.01

--- Source/RimWorldOnlineCity/UI/DevelopTest.cs
+++ Source/RimWorldOnlineCity/UI/DevelopTest.cs
@@ -10,6 +10,9 @@ using RimWorld;
 using RimWorld.Planet;
 using Model;
 using RimWorldOnlineCity.UI;
+using UnityEngine;
+using System.Reflection;
+//using RimWorldOnlineCity.MapRenderer;
 
 namespace RimWorldOnlineCity
 {
@@ -58,10 +61,31 @@ namespace RimWorldOnlineCity
         public bool Run()
         {
             return false;
+            /*
+            //тестирование скриншотов
             Log.Message("DevelopTest Run");
             Loger.Log("");
-            Loger.Log("DevelopTest Run");
-            
+            Loger.Log("DevelopTest Run ");
+            var path = Path.Combine(Loger.PathLog, "MapSnapshot");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            MapRendererMod.settings.path = path;
+            //var t = GameObject.Find("GameRoot").AddComponent<RenderMap>();
+            //var t = GameObject.Find("GameRoot").AddComponentExt("RenderMap");
+            var t = new RenderMap();
+            Loger.Log("DevelopTest w2 " + t.GetType().FullName);
+            RenderMap renderMap = t as RenderMap;
+            renderMap.Initialize();
+            renderMap.Render();
+            Loger.Log("DevelopTest End");
+            return true;
+
+            //тестирование биржы
+            Log.Message("DevelopTest Run");
+            Loger.Log("");
+            Loger.Log("DevelopTest Run");            
             var formm = new Dialog_Exchenge(Find.Maps[0]);
             Find.WindowStack.Add(formm);
             return true;
@@ -242,6 +266,141 @@ namespace RimWorldOnlineCity
         }
         */
             return true;
+        }
+    }
+
+    public static class ExtensionMethod
+    {
+        public static Component AddComponentExt(this GameObject obj, string scriptName)
+        {
+            Component cmpnt = null;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                //If call is null, make another call
+                cmpnt = _AddComponentExt(obj, scriptName, i);
+
+                //Exit if we are successful
+                if (cmpnt != null)
+                {
+                    break;
+                }
+            }
+
+
+            //If still null then let user know an exception
+            if (cmpnt == null)
+            {
+                Debug.LogError("Failed to Add Component");
+                return null;
+            }
+            return cmpnt;
+        }
+
+        private static Component _AddComponentExt(GameObject obj, string className, int trials)
+        {
+            //Any script created by user(you)
+            const string userMadeScript = "Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+            //Any script/component that comes with Unity such as "Rigidbody"
+            const string builtInScript = "UnityEngine, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+            //Any script/component that comes with Unity such as "Image"
+            const string builtInScriptUI = "UnityEngine.UI, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+            //Any script/component that comes with Unity such as "Networking"
+            const string builtInScriptNetwork = "UnityEngine.Networking, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+            //Any script/component that comes with Unity such as "AnalyticsTracker"
+            const string builtInScriptAnalytics = "UnityEngine.Analytics, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+            //Any script/component that comes with Unity such as "AnalyticsTracker"
+            const string builtInScriptHoloLens = "UnityEngine.HoloLens, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+            Assembly asm = null;
+
+            try
+            {
+                //Decide if to get user script or built-in component
+                switch (trials)
+                {
+                    case 0:
+
+                        asm = Assembly.Load(userMadeScript);
+                        break;
+
+                    case 1:
+                        //Get UnityEngine.Component Typical component format
+                        className = "UnityEngine." + className;
+                        asm = Assembly.Load(builtInScript);
+                        break;
+                    case 2:
+                        //Get UnityEngine.Component UI format
+                        className = "UnityEngine.UI." + className;
+                        asm = Assembly.Load(builtInScriptUI);
+                        break;
+
+                    case 3:
+                        //Get UnityEngine.Component Video format
+                        className = "UnityEngine.Video." + className;
+                        asm = Assembly.Load(builtInScript);
+                        break;
+
+                    case 4:
+                        //Get UnityEngine.Component Networking format
+                        className = "UnityEngine.Networking." + className;
+                        asm = Assembly.Load(builtInScriptNetwork);
+                        break;
+                    case 5:
+                        //Get UnityEngine.Component Analytics format
+                        className = "UnityEngine.Analytics." + className;
+                        asm = Assembly.Load(builtInScriptAnalytics);
+                        break;
+
+                    case 6:
+                        //Get UnityEngine.Component EventSystems format
+                        className = "UnityEngine.EventSystems." + className;
+                        asm = Assembly.Load(builtInScriptUI);
+                        break;
+
+                    case 7:
+                        //Get UnityEngine.Component Audio format
+                        className = "UnityEngine.Audio." + className;
+                        asm = Assembly.Load(builtInScriptHoloLens);
+                        break;
+
+                    case 8:
+                        //Get UnityEngine.Component SpatialMapping format
+                        className = "UnityEngine.VR.WSA." + className;
+                        asm = Assembly.Load(builtInScriptHoloLens);
+                        break;
+
+                    case 9:
+                        //Get UnityEngine.Component AI format
+                        className = "UnityEngine.AI." + className;
+                        asm = Assembly.Load(builtInScript);
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                //Debug.Log("Failed to Load Assembly" + e.Message);
+            }
+
+            //Return if Assembly is null
+            if (asm == null)
+            {
+                return null;
+            }
+
+            //Get type then return if it is null
+            Type type = asm.GetType(className);
+            if (type == null)
+                return null;
+
+            //Finally Add component since nothing is null
+            Component cmpnt = obj.AddComponent(type);
+            return cmpnt;
         }
     }
 }
--- Source/RimWorldOnlineCity/UnionDll/MainHelper.cs
+++ Source/RimWorldOnlineCity/UnionDll/MainHelper.cs
@@ -20,7 +20,7 @@ namespace OCUnion
         public static bool OffAllLog = false;
 
         //public static string VersionInfo = $"Version {Assembly.GetExecutingAssembly().FullName}";
-        public static string VersionInfo = "Version 0.03.45a from 2020.07.30";
+        public static string VersionInfo = "Version 0.03.45a from 2020.08.01";
 
         /// <summary>
         /// Для автоматической проверки: версия клиента должна быть больше или равна версии сервера
