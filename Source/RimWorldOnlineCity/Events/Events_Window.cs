457f8673a9a907ee9b20e1ba14372bc521dca9a5
M_Kovalchuk <babur1189@gmail.com>
2021-01-15 23:00:55 +0700

    development events control

---
+++ Source/RimWorldOnlineCity/Events/Events_Window.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace Window
+{
+    class Events_Window : MainTabWindow  // ui для отладки ивентов, нужно будет убрать
+    {
+        public override Vector2 InitialSize => new Vector2(600, 400);
+
+        public override void DoWindowContents(Rect inRect) //окно с кнопками вызовов ивентов
+        {
+            base.DoWindowContents(inRect);
+            resizeable = false;
+            forcePause = false;
+            doCloseX = true;
+
+            if (Widgets.ButtonText(new Rect(0, 10, 250, 35), $"do_Test_Event"))
+            {
+                var incident_pod = new OC_Incidents.Test_Incident();
+                if (!incident_pod.TryExecuteEvent())
+                {
+                    Messages.Message($"FailedTestEvent", MessageTypeDefOf.RejectInput);
+                }
+            }
+            if (Widgets.ButtonText(new Rect(0, 55, 250, 35), $"do_Test_raid"))
+            {
+                var raid_incident = new OC_Incidents.Raid_worker();
+                if (!raid_incident.TryExecuteEvent())
+                {
+                    Messages.Message($"FailedTestRaid", MessageTypeDefOf.RejectInput);
+                }
+            }
+        }
+    }
+}
---
+++ Source/RimWorldOnlineCity/Events/Test_Incident.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace OC_Incidents
+{
+	public class Test_Incident : IncidentWorker //самопальный ивент. всё равно наследование от того же интерфейса что и у остальных
+	{
+		public bool TryExecuteEvent()
+        {
+			IncidentParms parms = new IncidentParms()
+			{
+				customLetterLabel = "test event",
+				customLetterText = "teast event again",
+				target = Current.Game.AnyPlayerHomeMap,
+				points = StorytellerUtility.DefaultThreatPointsNow(Find.CurrentMap),
+			};
+			return TryExecuteWorker(parms);
+        }
+		protected override bool TryExecuteWorker(IncidentParms parms) //подобие ивента с грузовыми капсулами
+		{
+			Map map = (Map)parms.target;
+			List<Thing> things = ThingSetMakerDefOf.ResourcePod.root.Generate(); //заменить вещи?
+			IntVec3 intVec = DropCellFinder.RandomDropSpot(map);
+			DropPodUtility.DropThingsNear(intVec, map, things, 110, canInstaDropDuringInit: false, leaveSlag: true);
+			Find.LetterStack.ReceiveLetter("I can do my own events!", "some text", LetterDefOf.PositiveEvent, new TargetInfo(intVec, map));
+			return true;
+		}
+	}
+
+}
